7659570fe10fd1bc7750ccd604673391d5138b30
superman <husteritc@gmail.com>
2017-09-26 22:28:41 +0800

    fix link error

--- src/Init/IListener.h
+++ src/Init/IListener.h
@@ -11,15 +11,30 @@ public:
 	virtual void NotifyRenderFrame(GLfloat deltaTime) = 0;
 	virtual void NotifyEndFrame(GLfloat deltaTime) = 0;
 	virtual void NotifyReshape(int width, int height, int previousWidth, int previousHeight) = 0;
-    virtual void NotifyKeyInput(int key, int scancode, int action, int mode) = 0;
-    virtual void NotifyMouseMove(double xpos, double ypos) = 0;
-    virtual void NotifyScrollMove(double xoffset, double yoffset) = 0;
+    virtual void NotifyKeyInput(int key, int scancode, int action, int mode){};
+    virtual void NotifyMouseMove(double xpos, double ypos){};
+    virtual void NotifyScrollMove(double xoffset, double yoffset){};
 };
 
 inline IListener::~IListener()
 {
 
 }
+
+//void NotifyKeyInput(int key, int scancode, int action, int mode)
+//{
+//    
+//}
+//
+//void NotifyMouseMove(double xpos, double ypos)
+//{
+//    
+//}
+//
+//void NotifyScrollMove(double xoffset, double yoffset)
+//{
+//    
+//}
 
 
 #endif
