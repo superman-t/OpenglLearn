996ac463c998722c71325824190607e421c09b37
SUPERMAN-T <husteritc@gmail.com>
2017-10-11 21:16:11 +0800

    .

--- src/Rendering/Models/Model.cpp
+++ src/Rendering/Models/Model.cpp
@@ -79,20 +79,15 @@ void Model::LoadModel(std::string& path)
 // Processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
 void Model::ProcessNode(aiNode* node, const aiScene* scene)
 {
-	// Process each mesh located at the current node
 	for (GLuint i = 0; i < node->mNumMeshes; i++)
 	{
-		// The node object only contains indices to index the actual objects in the scene. 
-		// The scene contains all the data, node is just to keep stuff organized (like relations between nodes).
 		aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
 		this->meshes.push_back(this->ProcessMesh(mesh, scene));
 	}
-	// After we've processed all of the meshes (if any) we then recursively process each of the children nodes
 	for (GLuint i = 0; i < node->mNumChildren; i++)
 	{
 		this->ProcessNode(node->mChildren[i], scene);
 	}
-
 }
 
 Mesh Model::ProcessMesh(aiMesh* mesh, const aiScene* scene)
@@ -102,65 +97,49 @@ Mesh Model::ProcessMesh(aiMesh* mesh, const aiScene* scene)
 	vector<GLuint> indices;
 	vector<Texture> textures;
 
-	// Walk through each of the mesh's vertices
-	for (GLuint i = 0; i < mesh->mNumVertices; i++)
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
 	{
 		Vertex vertex;
-		glm::vec3 vector; // We declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
-		
-						  // Positions
-		vector.x = mesh->mVertices[i].x;
-		vector.y = mesh->mVertices[i].y;
-		vector.z = mesh->mVertices[i].z;
-		vertex.position = vector;
-
-// 		// Normals
-// 		vector.x = mesh->mNormals[i].x;
-// 		vector.y = mesh->mNormals[i].y;
-// 		vector.z = mesh->mNormals[i].z;
-// 		vertex.normal = vector;
-
-// 		// Texture Coordinates
-// 		if (mesh->mTextureCoords[0]) // Does the mesh contain texture coordinates?
-// 		{
-// 			glm::vec2 vec;
-// 			// A vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
-// 			// use models where a vertex can have multiple texture coordinates so we always take the first set (0).
-// 			vec.x = mesh->mTextureCoords[0][i].x;
-// 			vec.y = mesh->mTextureCoords[0][i].y;
-// 			vertex.texCoords = vec;
-// 		}
-// 		else
-// 			vertex.texCoords = glm::vec2(0.0f, 0.0f);
+		if (mesh->HasPositions())
+			vertex.position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+		if (mesh->HasNormals())
+			vertex.normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+		if (mesh->mTextureCoords[0])
+		{
+			vertex.texCoords = glm::vec2(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y);
+		}
+		else
+		{
+			vertex.texCoords = glm::vec2(0.0f, 0.0f);
+		}
 		vertices.push_back(vertex);
 	}
-	
-	// Now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
+
 	for (GLuint i = 0; i < mesh->mNumFaces; i++)
 	{
 		aiFace face = mesh->mFaces[i];
-		// Retrieve all indices of the face and store them in the indices vector
 		for (GLuint j = 0; j < face.mNumIndices; j++)
 			indices.push_back(face.mIndices[j]);
 	}
-// 	// Process materials
-// 	if (mesh->mMaterialIndex >= 0)
-// 	{
-// 		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-// 		// We assume a convention for sampler names in the shaders. Each diffuse texture should be named
-// 		// as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
-// 		// Same applies to other texture as the following list summarizes:
-// 		// Diffuse: texture_diffuseN
-// 		// Specular: texture_specularN
-// 		// Normal: texture_normalN
-// 
-// 		// 1. Diffuse maps
-// 		vector<Texture> diffuseMaps = this->LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
-// 		textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-// 		// 2. Specular maps
-// 		vector<Texture> specularMaps = this->LoadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
-// 		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-// 	}
+
+	// Process materials
+	if (mesh->mMaterialIndex >= 0)
+	{
+		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+		// We assume a convention for sampler names in the shaders. Each diffuse texture should be named
+		// as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
+		// Same applies to other texture as the following list summarizes:
+		// Diffuse: texture_diffuseN
+		// Specular: texture_specularN
+		// Normal: texture_normalN
+
+		// 1. Diffuse maps
+		vector<Texture> diffuseMaps = this->LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+		textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+		// 2. Specular maps
+		vector<Texture> specularMaps = this->LoadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+		textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+	}
 
 	// Return a mesh object created from the extracted mesh data
 	return Mesh(vertices, indices, textures);
@@ -177,7 +156,10 @@ vector<Texture> Model::LoadMaterialTextures(aiMaterial* mat, aiTextureType type,
 		mat->GetTexture(type, i, &str);
 		// Check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
 		Texture texture;
-		texture.id = TextureFromFile(str.C_Str(), this->directory);
+		std::string path(str.C_Str());
+		if (path.find("materials") != string::npos)
+			path = path.substr(path.find("materials"), path.length());
+		texture.id = TextureFromFile(path.c_str(), this->directory);
 		texture.type = typeName;
 		texture.path = str;
 		textures.push_back(texture);
